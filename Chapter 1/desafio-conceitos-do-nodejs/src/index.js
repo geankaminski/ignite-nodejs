33077dfc558762996c6f7de3f4a557e3f74c4d61
Gean Kaminski <geankaminski93@hotmail.com>
2021-04-29 09:00:07 -0300

    rentalx project created

--- Chapter 1/desafio-conceitos-do-nodejs/src/index.js
+++ Chapter 1/desafio-conceitos-do-nodejs/src/index.js
@@ -14,7 +14,7 @@ function checksExistsUserAccount(request, response, next) {
     const { username } = request.headers;
     const user = users.find((user) => user.username === username);
     if(!user){
-        return response.status(400).json({ error: "User not found!"})
+        return response.status(404).json({ error: "User not found!"})
     }
     request.user = user;
     return next();
--- Chapter 1/desafio-trabalhando-com-middlewares/src/index.js
+++ Chapter 1/desafio-trabalhando-com-middlewares/src/index.js
@@ -10,19 +10,53 @@ app.use(cors());
 const users = [];
 
 function checksExistsUserAccount(request, response, next) {
-  // Complete aqui
+  const { username } = request.headers;
+  const user = users.find(user => user.username === username)
+  if (!user) {
+    return response.status(404).json({ error: "User not found!"})
+  }
+  request.user = user
+  next();
 }
 
 function checksCreateTodosUserAvailability(request, response, next) {
-  // Complete aqui
+  const user = request.user;
+  const numberOfTodos = user.todos.length
+  if(user.pro || numberOfTodos < 10){
+    next()
+  }
+  return response.status(403).json({ error: "User is not Pro!"})
 }
 
 function checksTodoExists(request, response, next) {
-  // Complete aqui
+  const { username } = request.headers;
+  const { id } = request.params;
+  const isUuid = validate(id);
+  if(!isUuid) {
+    return response.status(400)
+  }
+  const user = users.find(user => user.username === username);
+  if(!user) {
+    return response.status(404)
+  }
+  const todo = user.todos.find(todo => todo.id === id);
+  
+  if (isUuid && todo) {
+    request.todo = todo
+    request.user = user
+    next()
+  }
+  return response.status(404)
 }
 
 function findUserById(request, response, next) {
-  // Complete aqui
+  const { id } = request.params;
+  const user = users.find(user => user.id === id)
+  if (!user) {
+    return response.status(404).json({ error: "User not found!"})
+  }
+  request.user = user
+  next();
 }
 
 app.post('/users', (request, response) => {
