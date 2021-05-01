93c4d8513a5c84b0731b53f76318f0177af3ff15
Gean Kaminski <geankaminski93@hotmail.com>
2021-05-01 00:12:59 -0300

    Stream files

--- Chapter 2/rentalx/src/routes/categories.routes.ts
+++ Chapter 2/rentalx/src/routes/categories.routes.ts
@@ -1,27 +1,26 @@
 import { Router } from "express";
+import multer from "multer";
 
-import { CategoriesRepository } from "../repositories/CategoriesRepository";
-import { CreateCategoryService } from "../services/CreateCategoryService";
+import { createCategoryController } from "../modules/cars/useCases/createCategory";
+import { importCategoryController } from "../modules/cars/useCases/importCategory";
+import { listCategoriesController } from "../modules/cars/useCases/listCategories";
 
 const categoriesRoutes = Router();
-const categoriesRepository = new CategoriesRepository();
+
+const upload = multer({
+    dest: "./tmp",
+});
 
 categoriesRoutes.post("/", (request, response) => {
-    const { name, description } = request.body;
-
-    const createCategoryService = new CreateCategoryService(
-        categoriesRepository
-    );
-
-    createCategoryService.execute({ name, description });
-
-    return response.status(201).send();
+    return createCategoryController.handle(request, response);
 });
 
 categoriesRoutes.get("/", (request, response) => {
-    const all = categoriesRepository.list();
+    return listCategoriesController.handle(request, response);
+});
 
-    return response.json(all);
+categoriesRoutes.post("/import", upload.single("file"), (request, response) => {
+    return importCategoryController.handle(request, response);
 });
 
 export { categoriesRoutes };
