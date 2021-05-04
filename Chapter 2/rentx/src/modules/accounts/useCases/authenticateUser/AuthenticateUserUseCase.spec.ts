ffe26ef6ae5eecaa2d6365e10f34715cea90f5e5
Gean Kaminski <geankaminski93@hotmail.com>
2021-05-04 19:41:11 -0300

    adicionando testes de autenticação

---
+++ Chapter 2/rentx/src/modules/accounts/useCases/authenticateUser/AuthenticateUserUseCase.spec.ts
@@ -0,0 +1,35 @@
+import { ICreateUserDTO } from "../../dtos/ICreateUserDTO";
+import { UsersRepositoryInMemory } from "../../repositories/in-memory/UsersRepositoryInMemory";
+import { CreateUserUseCase } from "../createUser/CreateUserUseCase";
+import { AuthenticateUserUseCase } from "./AuthenticatUserUseCase";
+
+let authenticateUserUseCase: AuthenticateUserUseCase;
+let usersRepositoryInMemory: UsersRepositoryInMemory;
+let createUserUseCase: CreateUserUseCase;
+
+describe("Authenticate User", () => {
+    beforeEach(() => {
+        usersRepositoryInMemory = new UsersRepositoryInMemory();
+        authenticateUserUseCase = new AuthenticateUserUseCase(
+            usersRepositoryInMemory
+        );
+        createUserUseCase = new CreateUserUseCase(usersRepositoryInMemory);
+    });
+
+    it("should be able to authenticate an user", async () => {
+        const user: ICreateUserDTO = {
+            driver_license: "00123",
+            email: "user@test.com",
+            password: "1234",
+            name: "John Doe",
+        };
+        await createUserUseCase.execute(user);
+
+        const result = await authenticateUserUseCase.execute({
+            email: user.email,
+            password: user.password,
+        });
+
+        expect(result).toHaveProperty("token");
+    });
+});
--- Chapter 2/rentx/src/routes/index.ts
+++ Chapter 2/rentx/src/routes/index.ts
@@ -3,7 +3,7 @@ import { Router } from "express";
 import { authenticateRoutes } from "./authenticate.routes";
 import { categoriesRoutes } from "./categories.routes";
 import { specificationsRoutes } from "./specifications.routes";
-import { usersRoutes } from "./uses.routes";
+import { usersRoutes } from "./users.routes";
 
 const router = Router();
 
