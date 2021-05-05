2a54bf0784e5351ca29bbc5f020ae9f54405a869
Gean Kaminski <geankaminski93@hotmail.com>
2021-05-05 15:17:34 -0300

    autenticação e usuário admin

---
+++ Chapter 2/rentx/src/shared/infra/http/routes/cars.routes.ts
@@ -0,0 +1,19 @@
+import { Router } from "express";
+
+import { CreateCarController } from "@modules/cars/useCases/createCar/CreateCarController";
+
+import { ensureAdmin } from "../middlewares/ensureAdmin";
+import { ensureAuthenticated } from "../middlewares/ensureAuthenticated";
+
+const carsRoutes = Router();
+
+const createCarController = new CreateCarController();
+
+carsRoutes.post(
+    "/",
+    ensureAuthenticated,
+    ensureAdmin,
+    createCarController.handle
+);
+
+export { carsRoutes };
